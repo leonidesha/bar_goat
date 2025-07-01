4c2bbe8803eb1e13833143bad8fdf448273bda82
Leonides Hidalgo <leonideha@gmail.com>
2025-07-01 11:55:44 -0400

    actualizando

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  vite: {
+    server: {
+      allowedHosts: ['127.0.0.1'],
+    }
+}
 })
